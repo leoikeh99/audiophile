39db9de6dff8437cce24c3a492bd4a707c39b382
Ikeh Leonard <leoikeh99@gmail.com>
2022-04-08 18:01:02 +0100

    Made deployment fixes

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -21,10 +21,10 @@ export default function index() {
           </nav>
         </F.Top>
         <F.Text mb>
-          Audiophile is an all in one stop to fulfill your audio needs. We're a
-          small team of music lovers and sound specialists who are devoted to
-          helping you get the most out of personal audio. Come and visit our
-          demo facility - we're open 7 days a week.
+          Audiophile is an all in one stop to fulfill your audio needs.
+          We&apos;re a small team of music lovers and sound specialists who are
+          devoted to helping you get the most out of personal audio. Come and
+          visit our demo facility - we&apos;re open 7 days a week.
         </F.Text>
         <F.Bottom>
           <F.Text>Copyright 2021. All Rights Reserved</F.Text>
